08cdc79cce3a9e3b8273cf97363a053a40f88693
florent.bouchard@centralesupelec.fr <florent.bouchard@centralesupelec.fr>
2025-12-15 18:19:59 +0100

    added scalar std for log-Euclidean

--- src/yetanotherspdnet/functions/spd_geometries/log_euclidean.py
+++ src/yetanotherspdnet/functions/spd_geometries/log_euclidean.py
@@ -1,7 +1,13 @@
-from pytest import Function
 import torch
+from torch.autograd import Function
 
-from ..spd_linalg import ExpmSymmetric, LogmSPD, expm_symmetric, logm_SPD
+from ..spd_linalg import (
+    ExpmSymmetric,
+    LogmSPD,
+    eigh_operation_grad,
+    expm_symmetric,
+    logm_SPD,
+)
 from .kullback_leibler import (
     EuclideanGeodesic,
     euclidean_geodesic,
@@ -114,3 +120,116 @@ def LogEuclideanMean(data: torch.Tensor) -> torch.Tensor:
     log_mean = ArithmeticMean.apply(log_data)
     mean = ExpmSymmetric.apply(log_mean)
     return mean
+
+
+# ---------------
+# Scalar variance
+# ---------------
+def log_euclidean_std_scalar(
+    data: torch.Tensor, reference_point: torch.Tensor
+) -> torch.Tensor:
+    """
+    Scalar standard deviation with respect to the Log-Euclidean distance
+
+    Parameters
+    ----------
+    data : torch.Tensor of shape (..., n_features, n_features)
+        Batch of SPD matrices
+
+    reference_point : torch.Tensor of shape (n_features, n_features)
+        SPD matrix (some kind of mean of data)
+
+    Returns
+    -------
+    scalar_std : torch.Tensor of shape ()
+        scalar standard deviation
+    """
+    n_matrices = torch.prod(torch.tensor(data.shape[:-2]))
+    logm_data = logm_SPD(data)[0]
+    logm_G = logm_SPD(reference_point)[0]
+    return torch.sqrt(torch.sum((logm_data - logm_G) ** 2) / n_matrices)
+
+
+class LogEuclideanStdScalar(Function):
+    """
+    Scalar standard deviation with respect to the Log-Euclidean distance
+    """
+
+    @staticmethod
+    def forward(ctx, data: torch.Tensor, reference_point: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the scalar standard deviation with respect to the Log-Euclidean distance
+
+        Parameters
+        ----------
+        ctx : torch.autograd.function._ContextMethodMixin
+            Context object to retrieve tensors saved during the forward pass
+
+        data : torch.Tensor of shape (..., n_features, n_features)
+            Batch of SPD matrices
+
+        reference_point : torch.Tensor of shape (n_features, n_features)
+            SPD matrix (some kind of mean of data)
+
+        Returns
+        -------
+        scalar_std : torch.Tensor of shape ()
+            scalar standard deviation
+        """
+        n_matrices = torch.prod(torch.tensor(data.shape[:-2]))
+        logm_data, eigvals_data, eigvecs_data = logm_SPD(data)
+        logm_G, eigvals_G, eigvecs_G = logm_SPD(reference_point)
+        std_scalar = torch.sqrt(torch.sum((logm_data - logm_G) ** 2) / n_matrices)
+        ctx.n_matrices = n_matrices
+        ctx.save_for_backward(
+            logm_data,
+            logm_G,
+            eigvals_data,
+            eigvecs_data,
+            eigvals_G,
+            eigvecs_G,
+            std_scalar,
+        )
+        return std_scalar
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Backward pass of the scalar standard deviation with respect to the affine-invariant distance
+
+        Parameters
+        ----------
+        ctx : torch.autograd.function._ContextMethodMixin
+            Context object to retrieve tensors saved during the forward pass
+
+        grad_output : torch.Tensor of shape ()
+            Gradient of the loss with respect to the output of the scalar standard deviation Function
+
+        Returns
+        -------
+        grad_input_data : torch.Tensor of shape (..., n_features, n_features)
+            gradient of the loss with respect to the input data
+
+        grad_input_reference_point : torch.Tensor of shape (n_features, n_features)
+            gradient of the loss with respect to the input reference point
+        """
+        n_matrices = ctx.n_matrices
+        (
+            logm_data,
+            logm_G,
+            eigvals_data,
+            eigvecs_data,
+            eigvals_G,
+            eigvecs_G,
+            std_scalar,
+        ) = ctx.saved_tensors
+        grad_logm_data = grad_output * (logm_data - logm_G) / std_scalar / n_matrices
+        grad_logm_G = grad_output * (logm_G - arithmetic_mean(logm_data)) / std_scalar
+        inv = lambda x: 1 / x
+        grad_input_data = eigh_operation_grad(
+            grad_logm_data, eigvals_data, eigvecs_data, torch.log, inv
+        )
+        grad_input_G = eigh_operation_grad(
+            grad_logm_G, eigvals_G, eigvecs_G, torch.log, inv
+        )
+        return grad_input_data, grad_input_G
