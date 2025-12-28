b9f5bb1dc12695333295df9c0331ab9e5a85536e
Matthieu Gallet <matthieu.gallet@protonmail.com>
2025-12-29 00:14:14 +0100

    Switch to sphinxawesome-theme for better documentation styling

--- docs/conf.py
+++ docs/conf.py
@@ -85,25 +85,36 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = "alabaster"
+html_theme = "sphinxawesome_theme"
 html_static_path = ["_static"]
 html_title = "Yet Another SPDNet"
 
-# Alabaster theme options
+# Sphinx Awesome Theme options
 html_theme_options = {
-    "description": "A robust implementation of SPDNet for SPD matrices",
-    "github_user": "Yet-Another-Research-Organisation",
-    "github_repo": "yetanotherspdnet",
-    "github_banner": True,
-    "github_button": True,
-    "github_type": "star",
-    "show_related": False,
-    "note_bg": "#FFF59C",
-    "show_powered_by": False,
-    "sidebar_collapse": True,
-    "extra_nav_links": {
-        "GitHub": "https://github.com/Yet-Another-Research-Organisation/yetanotherspdnet",
-        "Issue Tracker": "https://github.com/Yet-Another-Research-Organisation/yetanotherspdnet/issues",
+    "show_breadcrumbs": True,
+    "show_prev_next": True,
+    "show_scrolltop": True,
+    "extra_header_link_icons": {
+        "GitHub": {
+            "link": "https://github.com/Yet-Another-Research-Organisation/yetanotherspdnet",
+            "icon": (
+                '<svg height="26px" style="margin-top:-2px;display:inline" '
+                'viewBox="0 0 45 44" '
+                'fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+                '<path fill-rule="evenodd" clip-rule="evenodd" '
+                'd="M22.477.927C10.485.927.76 10.65.76 22.647c0 9.596 6.223 17.736 '
+                '14.853 20.608 1.087.2 1.483-.47 1.483-1.047 0-.516-.019-1.881-.03-3.697-6.04 '
+                '1.31-7.315-2.91-7.315-2.91-.988-2.51-2.412-3.178-2.412-3.178-1.972-1.346.149-1.32.149-1.32 '
+                '2.18.154 3.327 2.24 3.327 2.24 1.937 3.318 5.084 2.36 6.321 1.803.197-1.403.739-2.36 '
+                '1.349-2.903-4.735-.542-9.713-2.367-9.713-10.542 0-2.33.828-4.233 2.19-5.73-.221-.544-.961-2.74.206-5.713 '
+                '0 0 1.795-.573 5.885 2.193 1.705-.48 3.547-.72 5.37-.72 1.823 0 3.665.24 5.37.72 4.09-2.766 '
+                '5.885-2.193 5.885-2.193.367 2.973-.027 5.169.206 5.713 1.362 1.497 2.19 3.4 2.19 5.73 0 '
+                '8.216-4.995 10.001-9.76 10.542.76.694 1.44 2.063 1.44 4.158 0 3.007-.057 5.437-.075 6.178.909.52 '
+                '1.52 1.21 1.52 2.58 0 1.86-.27 3.32-.81 3.86C41.7 40.683 48 32.54 48 22.647 48 10.65 38.28.927 '
+                '26.29.927z" fill="currentColor"/>'
+                '</svg>'
+            ),
+        }
     },
 }
 
